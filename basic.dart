4c6fab07af3bc26852c7503f2d4eaa2dd885e32a
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2023-01-13 21:14:47 +0600

    dart Map practice

--- basic.dart
+++ basic.dart
@@ -168,6 +168,71 @@ void main(){
  // });
 
 
+  ///////////======== Map ==============/////////////
+
+  //==== Map Literal ======
+  // var info = {'name': 'Md Nazmul Hasan','age': '25','Upazilla': 'Harinakundu'};
+
+  // print(info['name']);
+  // print(info);
+
+  //adding value at runtime
+  // info['district'] = 'Jhenaidah';
+  // info['division'] = 'Khulna';
+  // print(info);
+
+
+  //========== Map Constructor ===========
+  // var info = new Map();
+
+  //==== Add value in Map
+  // info['name'] = 'Md Nazmul Hasan';
+  // info['age'] = 25;
+  // info['address'] = 'Harinakundu, Jhenaidah';
+
+  //=========== Add and Remove value from Map
+  // info.addAll({'name': 'Md Nazmul Hasan','age': 25,'address': 'Narayankandi'});
+
+  // info.remove('address');
+  // info.clear();
+
+  // print(info);
+
+  // var info = new Map();
+
+  // info.addAll({'name': 'Md Nazmul Hasan','age': 25,'address': 'Narayankandi'});
+
+  // print(info);
+
+
+  // =========== Map Property ==========
+  // var info = new Map();
+
+  // info.addAll({'name': 'Md Nazmul Hasan','age': 25,'address': 'Narayankandi'});
+
+
+  // print(info.length);
+  // print(info.keys);
+  // print(info.values);
+  // print(info.isEmpty);
+  // print(info.isNotEmpty);
+
+
+  ///============ Map Iterating =============
+
+  var info = new Map();
+
+  info.addAll({'name': 'Md Nazmul Hasan','age': 25,'address': 'Narayankandi'});
+
+  // print(info['name']);
+
+  info.forEach((key, value) {
+    // print(key);
+    // print(value);
+    print('${key}: ${value}');
+  });
+
+
 
 
 
