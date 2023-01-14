105e27971de1e80687e629368dc77b7ae1758e5b
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2023-01-14 10:07:36 +0600

    dart loop

---
+++ loop/loop.dart
@@ -0,0 +1,58 @@
+void main(){
+
+  //====== For Loop ==========
+
+  // for(var i=1; i<=10; i++){
+    // print("Dart Loop: ${i}");
+  // }
+
+  //======== While Loop ==========
+
+  // var i = 1;
+
+  // while(i<=10){
+    // print("Dart Loop: ${i}");
+    // i++;
+  // }
+
+  //====== Do While Loop =======
+
+  // var i = 1;
+  // do{
+  //   print("Dart Loop: ${i}");
+  //   i++;
+  // }while(i<=10);
+
+
+  //======= For In Loop =========
+
+  // var students = ['Nazmul','Rakib','Tisha','Monir'];
+  //
+  // for(var student in students){
+  //   print(student);
+  // }
+
+
+  //======== Continue and Break Statement
+
+  // for(var i=1; i<=10; i++){
+  //   if(i == 8){
+  //     continue;
+  //   }
+  //
+  //   print(i);
+  //
+  // }
+  //
+  // for(var i=1; i<=10; i++){
+  //   if(i == 8){
+  //     break;
+  //   }
+  //
+  //   print(i);
+  //
+  // }
+
+
+
+}
