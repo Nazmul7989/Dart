39bdecf2fe08996845bbf6b3cb56df8746de272d
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2023-01-20 19:22:04 +0600

    function practice

--- function/dart.dart
+++ function/dart.dart
@@ -1,9 +1,20 @@
 void main(){
 
-  stringFunction();
-
+  // test();
+  // print(stringFunction());
+  print(integerFunction());
 }
 
-void stringFunction(){
-  print('Hello function');
+void test(){
+  print('Hello Dart Function');
+}
+
+String stringFunction(){
+  return 'Dart String Function';
+}
+
+int integerFunction(){
+  int a = 20;
+  int b = 30;
+  return a+b;
 }
