db8bbc9e0581f612536871024e1bf116c1ef0fc4
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2023-01-21 12:47:38 +0600

    dart function

--- function/dart.dart
+++ function/dart.dart
@@ -2,7 +2,9 @@ void main(){
 
   // test();
   // print(stringFunction());
-  print(integerFunction());
+  // print(integerFunction());
+  // print(integerFunctionWithParameter(20, 30));
+  print(listFunction());
 }
 
 void test(){
@@ -17,4 +19,14 @@ int integerFunction(){
   int a = 20;
   int b = 30;
   return a+b;
+}
+
+int integerFunctionWithParameter(int a, int b){
+  return a+b;
+}
+
+List listFunction(){
+  var list = [];
+  list.addAll(['Nazmul','Kamal','Rahim','Mizan']);
+  return list;
 }
